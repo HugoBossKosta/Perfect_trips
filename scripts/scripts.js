52597af904768f4d269ffda2d9cbaaed8dccf76b
Konstantin Kuznetsov <hugobossscorpion@gmail.com>
2022-11-26 21:44:30 +0300

    itog header + underheader

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,52 @@
 // изменение цвета скрола
 
-document.addEventListener('resize', () => {
-    if (window.scrollY > 450 && heder.style.color != 'red') {
-      header.style.color = 'red'
-    } else if (header.style.color == 'red') header.style.color = 'auto'
-  })
+// document.addEventListener('resize', () => {
+//     if (window.scrollY > 450 && heder.style.color != 'red') {
+//       header.style.color = 'red'
+//     } else if (header.style.color == 'red') header.style.color = 'auto'
+//   })
+
+
+//появление Header при прокрутке
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const onScrollHeader = () => {
+
+        const header = document.querySelector('.header-block');
+        let prevScroll = window.pageYOffset;
+        let currentScroll;
+
+        window.onload = () => {
+            header.classList.add('header-block__hidden');
+        }
+        window.addEventListener('scroll', () => { 
+            currentScroll = window.pageYOffset;
+
+
+            if (window.scrollY > 450) {
+
+                header.classList.remove('header-block__hidden');
+                
+                // если нужно сделать, чтобы скролл убирался динамически при измении колеса мыши
+
+                // const headerHidden = () => header.classList.contains('header-block__hidden');
+
+                // if (currentScroll > prevScroll && !headerHidden()) { 
+                //     header.classList.add('header-block__hidden');
+                // }
+                // if (currentScroll < prevScroll && headerHidden()) { 
+                //     header.classList.remove('header-block__hidden') ;
+                // }
+
+                // prevScroll = currentScroll;
+            }
+            else {
+                header.classList.add('header-block__hidden');
+            }
+
+        });
+    }
+    onScrollHeader();
+
+});
+
