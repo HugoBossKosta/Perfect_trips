faf2389bdd34e567909c63a6381e961b5bbdaf4d
Konstantin Kuznetsov <hugobossscorpion@gmail.com>
2022-12-25 16:02:05 +0300

    element-animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -147,3 +147,23 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// Всплытие элементов при прокрутке
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.2] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+
+
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
